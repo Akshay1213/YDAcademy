31d7bd6f4a4b271bf6cecafdf5412aec97f4b25c
Akshay1213 <akshaysaindane10@gmail.com>
2017-11-17 10:45:15 +0530

    Activity main updated

--- app/src/main/java/com/example/user/ydacademy/ContactUs.java
+++ app/src/main/java/com/example/user/ydacademy/ContactUs.java
@@ -24,75 +24,8 @@ public class ContactUs extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_us);
         actionBarSetup();
-     /*   b1 = (Button) findViewById(R.id.btnSubmit);
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-                msg = "";
-                e1 = (EditText) findViewById(R.id.name);
-                String name = e1.getText().toString();
-                if (name.length() == 0) {
-                    e1.setError("Please enter name");
-                    flag = 1;
-                } else {
-                    msg += "Name:" + e1.getText() + "\n";
-                }
-                Spinner spinnerClass = (Spinner) findViewById(R.id.spinner1);
-                msg = "Class:" + spinnerClass.getSelectedItem().toString() + "\n";
-                Log.d("Spinner", spinnerClass.getSelectedItem().toString());
 
-                e1 = (EditText) findViewById(R.id.email);
-                String mail = e1.getText().toString();
-                String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
-                if (!mail.matches(emailPattern)) {
-                    e1.setError("Invalid email");
-                    flag = 1;
-                } else {
-                    msg += "Email:" + e1.getText() + "\n";
-                }
-
-                e1 = (EditText) findViewById(R.id.phone);
-
-                String number = e1.getText().toString();
-                if (number.length() < 10 || number.length() == 0) {
-                    e1.setError("Invalid number");
-                    flag = 1;
-                } else {
-                    msg += "Phone No:" + e1.getText() + "\n";
-                }
-
-                e1 = (EditText) findViewById(R.id.description);
-                String comment = e1.getText().toString();
-                if (comment.length() == 0) {
-                    e1.setError("Please enter comment");
-                    flag = 1;
-                } else {
-                    msg += "Comment:" + e1.getText() + "\n";
-                }
-                if (flag == 0) {
-                    String[] to = {"ostallohostels@gmail.com"};
-                    Intent intent = new Intent(Intent.ACTION_SEND);
-                    intent.setType("text/html");
-                    intent.putExtra(Intent.EXTRA_EMAIL, to);
-                    intent.putExtra(Intent.EXTRA_SUBJECT, "Contact Us");
-                    intent.putExtra(Intent.EXTRA_TEXT, msg);
-
-                    try {
-                        startActivity(Intent.createChooser(intent, "Send Email"));
-                        // Snackbar.make(findViewById(R.id.drawer_layout), "Mail sent", Snackbar.LENGTH_LONG).setAction("Action", null).show();
-                        Log.d("******", "Finished sending email...");
-                    } catch (android.content.ActivityNotFoundException ex) {
-                        Toast.makeText(ContactUs.this, "Mail not sent", Toast.LENGTH_SHORT).show();
-                    }
-                   *//* finally {
-                        finish();
-                    }*//*
-                }
-                flag = 0;
-
-            }
-        });*/
     }
 
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
